4eafdf4a97f475e5bcdfa0386be4995bd5ec61a5
GK143GK <sgopi.mba@gmail.com>
2022-03-03 11:37:15 +0530

    version1

---
+++ front-end/src/index.js
@@ -0,0 +1,12 @@
+import react from 'react'
+import reactDom from 'react-dom'
+// render("what to show and where to show")
+reactDom.render(
+<>
+    <h1>Welcome to my World</h1>
+    <p>Gopikrishna</p>
+    <p>Shanmugan</p>
+    <p>Hi Dhanasekar</p>
+</>
+,document.getElementById("root")
+)
